10ef170ced1522f2c88b15e5e3439871d37a2c0a
sev7e0 <sev7e0@gmail.com>
2019-05-30 00:35:11 +0800

    SingleByEnum

---
+++ src/main/java/com/tools/java/SingleByEnum.java
@@ -0,0 +1,36 @@
+package com.tools.java;
+
+/**
+ * 使用枚举类型实现单例模式
+ */
+
+public enum SingleByEnum {
+    INSTANCE;
+    private Resource instance;
+    SingleByEnum() {
+        instance = new Resource();
+    }
+    public Resource getInstance() {
+        return instance;
+    }
+    class Resource{
+        private Resource() {
+        }
+
+        public void prit(){
+            System.out.println("get resource instance");
+        }
+    }
+}
+
+
+
+class eme{
+
+    public static void main(String[] args) {
+        SingleByEnum.Resource resource = SingleByEnum.INSTANCE.getInstance();
+        resource.prit();
+
+    }
+
+}
