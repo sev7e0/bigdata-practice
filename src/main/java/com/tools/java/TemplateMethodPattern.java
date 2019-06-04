e8a2b91b5003b7f1defcd0d429c79933ea2d7fb7
sev7e0 <sev7e0@gmail.com>
2019-06-04 21:54:58 +0800

    《effective java》TemplateMethodPattern

---
+++ src/main/java/com/tools/java/TemplateMethodPattern.java
@@ -0,0 +1,92 @@
+package com.tools.java;
+
+/**
+ * 模版方法模式
+ */
+public class TemplateMethodPattern {
+    public static void main(String[] args) {
+        MacBook macBook = new MacBook();
+        IPad iPad = new IPad();
+        macBook.startComputer();
+        iPad.startComputer();
+    }
+}
+
+interface Computer{
+
+    void power();
+
+    void mainBoard();
+
+    void cpu();
+
+    void hardDisk();
+
+    void display();
+}
+
+abstract class AbstractComputer implements Computer{
+
+    public void startComputer(){
+        power();
+        mainBoard();
+        cpu();
+        hardDisk();
+        display();
+
+    }
+
+    public final void power(){
+        System.out.println("开始供电");
+    }
+
+    public abstract void mainBoard();
+
+    public abstract void cpu();
+
+    public abstract void hardDisk();
+
+    public final void display(){
+        System.out.println("已启动");
+    }
+}
+
+class MacBook extends AbstractComputer implements Computer{
+
+
+    @Override
+    public void mainBoard() {
+        System.out.println("MacBook启动主板");
+    }
+
+    @Override
+    public void cpu() {
+        System.out.println("MacBook开始为CPU供电");
+    }
+
+    @Override
+    public void hardDisk() {
+        System.out.println("MacBook开始为硬盘供电");
+    }
+
+}
+
+class IPad extends AbstractComputer implements Computer{
+
+
+    @Override
+    public void mainBoard() {
+        System.out.println("IPad启动主板");
+    }
+
+    @Override
+    public void cpu() {
+        System.out.println("IPad开始为CPU供电");
+    }
+
+    @Override
+    public void hardDisk() {
+        System.out.println("IPad开始为硬盘供电");
+    }
+
+}
