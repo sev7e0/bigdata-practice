7229462240470f292ca8d47ba0fabaf2bedc1e17
sev7e0 <sev7e0@gmail.com>
2019-06-06 23:33:38 +0800

    zookeeper Java API

--- src/main/java/com/tools/zookeeper/client/DistributeClient.java
+++ src/main/java/com/tools/zookeeper/client/DistributeClient.java
@@ -1,54 +1,80 @@
 package com.tools.zookeeper.client;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import lombok.extern.slf4j.Slf4j;
 import org.apache.zookeeper.ZooKeeper;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
+@Slf4j
 public class DistributeClient {
 
-	private static final String connectString = "spark01:2181,spark02:2181,spark03:2181";
+    private static final String connectString = "localhost:2181";
 
-	private static final Integer sessionTimeout = 2000;
+    private static final Integer sessionTimeout = 2000;
 
-	private static final String parentNode = "/Servers";
+    private static final String parentNode = "/Servers";
 
-	private ZooKeeper zk = null;
+    private ZooKeeper zk = null;
 
-	private volatile List<String> serverList = null;
+    private volatile List<String> serverList = null;
 
-	public void getConnect() throws Exception {
-		zk = new ZooKeeper(connectString, sessionTimeout, event -> {
-			try {
-				getServerList();
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		});
-	}
+    private static CountDownLatch countDownLatch = new CountDownLatch(1);
 
-	public void getServerList() throws Exception {
-		List<String> children = zk.getChildren(parentNode, true);
+    public void getConnect() throws Exception {
+        zk = new ZooKeeper(connectString, sessionTimeout, event -> {
+            try {
+                log.info("链接成功,准备获取信息");
+                countDownLatch.countDown();
+                getServerList();
+            } catch (Exception e) {
+                log.error("获取信息失败！{}", e.getMessage());
+            }
+        });
+        countDownLatch.await();
+    }
 
-		List<String> list = new ArrayList<String>();
+    public void getServerList() throws Exception {
+        /**
+         * 读取数据，可以获取到节点列表和节点数据，
+         */
+//		List<String> children = zk.getChildren(parentNode, true);
+        List<String> children = zk.getChildren(parentNode, event -> {
+            try {
+                /**
+                 * 支持自定义Watch，在节点变更时会发送NodeChildrenChanged事件
+                 * 不过Watch仅一次有效
+                 */
+                log.info("此刻有节点变更事件产生！");
+                getServerList();
+            } catch (Exception e) {
+                log.error("注册的Watch调用失败。");
+            }
+        });
 
-		for (String child : children) {
-			byte[] data = zk.getData(parentNode + "/" + child, false, null);
-			list.add(new String(data));
-		}
-		serverList = list;
-		handllerService();
-	}
+        List<String> list = new ArrayList<>();
 
-	public void handllerService() {
-		System.out.println(serverList);
-	}
+        for (String child : children) {
+            /**
+             * 可以根据路径，获取节点中保存的数据，同样getChildren支持Watch注册
+             * 在节点数据发生变化时，可以发送事件。NodeDataChanged
+             */
+            byte[] data = zk.getData(parentNode + "/" + child, false, null);
+            list.add(new String(data));
+        }
+        serverList = list;
+        handlerService();
+    }
 
-	public static void main(String[] args) throws Exception {
-		DistributeClient client = new DistributeClient();
-		client.getConnect();
-		Thread.sleep(Long.MAX_VALUE);
-	}
+    public void handlerService() {
+        serverList.forEach(server -> log.info("当前在线服务有：{}", server));
+    }
+
+    public static void main(String[] args) throws Exception {
+        DistributeClient client = new DistributeClient();
+        client.getConnect();
+        Thread.sleep(Long.MAX_VALUE);
+    }
 
 }
--- src/main/java/com/tools/zookeeper/server/DistributeServer.java
+++ src/main/java/com/tools/zookeeper/server/DistributeServer.java
@@ -1,67 +1,86 @@
 package com.tools.zookeeper.server;
 
-import java.io.IOException;
-
+import lombok.extern.slf4j.Slf4j;
 import org.apache.zookeeper.CreateMode;
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooDefs.Ids;
 import org.apache.zookeeper.ZooKeeper;
 
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+
+@Slf4j
 public class DistributeServer {
 
-	//zookeeper连接地址
-	private static final String connectString = "spark01:2181,spark02:2181,spark03:2181";
-	//超时时间  用于zookeeper判断当前节点等待心跳最长时间
-	private static final Integer sessionTimeout = 2000;
-	//根节点
-	private static final String parentNode = "/Servers";
+    //zookeeper连接地址
+    private static final String connectString = "localhost:2181";
+    //超时时间  用于zookeeper判断当前节点等待心跳最长时间
+    private static final Integer sessionTimeout = 2000;
+    //根节点  ------ 启动前需要手动创建
+    private static final String parentNode = "/Servers";
 
-	private ZooKeeper zk = null;
+    private ZooKeeper zk = null;
 
-	/**
-	 * 1.getconnect 2.regserver 3.service
-	 * 
-	 * @throws IOException
-	 */
+    private static CountDownLatch countDownLatch = new CountDownLatch(1);
+    /**
+     * 1.getconnect 2.regserver 3.service
+     *
+     * @throws IOException
+     */
 
-	public void getConnect() throws Exception {
-		zk = new ZooKeeper(connectString, sessionTimeout, event -> {
+    /**
+     * 创建链接
+     * @throws Exception
+     */
+    public void getConnect() throws Exception {
+        zk = new ZooKeeper(connectString, sessionTimeout, event -> {
+            log.info("链接状态更改----"+event.getState());
+            countDownLatch.countDown();
+        });
+        /**
+         * 由于创建是异步的可能会导致链接未创建就执行
+         * 所以这里使用CountDownLatch进行阻塞
+         *
+         * 在链接创建后使用Watch进行解除阻塞。
+         */
+        countDownLatch.await();
+        //根据两个能够确定一个会话，可以实现客户端会话复用
+        log.info("sessionId为：{}",zk.getSessionId());
+        log.info("会话密钥为：{}",zk.getSessionPasswd());
 
-		});
-	}
 
-	/**
-	 * 
-	* @Title: regServer  
-	* @Description:  向zookeeper注册服务
-	* @param @param hostName
-	* @param @throws Exception
-	* @param @throws InterruptedException   
-	* @return void      
-	* @throws
-	 */
-		public void regServer(String hostName) throws Exception {
-			String creatPath = zk.create(parentNode + "/server", hostName.getBytes(), Ids.OPEN_ACL_UNSAFE,
-					CreateMode.EPHEMERAL_SEQUENTIAL);
-			System.out.println(hostName + "-------- is on line-----" + creatPath);
-		}
+    }
 
-		public void handleService(String hostName) {
-			System.out.println(hostName + " start working");
-		}
+    /**
+     * @param @param  hostName
+     * @param @throws Exception
+     * @param @throws InterruptedException
+     * @return void
+     * @throws
+     * @Title: regServer
+     * @Description: 向zookeeper注册服务
+     */
+    public void regServer(String hostName) throws Exception {
+        //支持异步创建，不支持递归创建，即不存在父节点的情况下不可以创建
+        String creatPath = zk.create(parentNode + "/server", hostName.getBytes(), Ids.OPEN_ACL_UNSAFE,
+                CreateMode.EPHEMERAL_SEQUENTIAL);
+        System.out.println(hostName + "-------- is on line-----" + creatPath);
+    }
 
-		public static void main(String[] args) throws Exception {
-			// TODO Auto-generated method stub
-			DistributeServer distributeServer = new DistributeServer();
+    public void handleService(String hostName) {
+        System.out.println(hostName + " start working");
+    }
 
-		distributeServer.getConnect();
+    public static void main(String[] args) throws Exception {
 
-		distributeServer.regServer(args[0]);
+        DistributeServer distributeServer = new DistributeServer();
 
-		distributeServer.handleService(args[0]);
+        distributeServer.getConnect();
 
-		Thread.sleep(Long.MAX_VALUE);
-	}
+        distributeServer.regServer(args[0]);
+
+        distributeServer.handleService(args[0]);
+
+        Thread.sleep(Long.MAX_VALUE);
+    }
 
 }
